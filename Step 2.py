a48944808b0091f19fb6f70538154d25feb5fe9c
Saad-ELH-ELB <75778999+Saad-ELH-ELB@users.noreply.github.com>
2023-07-21 22:08:56 +0200

    Update Step 2.py

--- Step 2.py
+++ Step 2.py
@@ -9,6 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from bidding_environnement_stp2 import *
 from GPTS_Learner_Step2 import *
+from GPUCB_Learner_Step3 import *
+from sklearn.exceptions import ConvergenceWarning
+import warnings
+
+
+# ignore scikit-learn warnings
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 n_arms = 100
 min_bid = 0
@@ -21,6 +28,7 @@ T = 365
 n_experiments = 500
 
 gpts_rewards_per_experiment = []
+gpucb_rewards_per_experiment= []
 
 
 #just an arbitrary value since we need to get it from a given curve 
@@ -31,24 +39,62 @@ cr = 0.6
 for e in range(0, n_experiments):
     env = BiddingEnvironment(bids=bids, sigma=sigma, margin=M, Conv_rate=cr)
     gpts_learner = GPTS_Learner(n_arms=n_arms, arms=bids)
+    gpucb_learner = GPUCB_Learner(n_arms=n_arms_adv, arms=bids)
     
     for t in range(0,T):
+         # GPTS Round
         pulled_arm = gpts_learner.pull_arm()
         reward = env.round(pulled_arm)
         gpts_learner.update(pulled_arm, reward)
+
+
+        # GPUCB Round
+        pulled_arm = gpucb_learner.pull_arm()
+        reward = env.round(pulled_arm)
+        gpucb_learner.update(pulled_arm_adv, reward_adv)
     
     gpts_rewards_per_experiment.append(gpts_learner.collected_rewards)
+    gpucb_rewards_per_experiment.append(gpucb_learner.collected_rewards)
     
     
-    
+print('GP-TS Profit: ',max(np.cumsum(np.mean(np.array(gpts_rewards_per_experiment),axis=0))))
+print('GP-UCB Profit: ',max(np.cumsum(np.mean(np.array(gpucb_rewards_per_experiment),axis=0))))
+
 opt = np.max(env.means)
-plt.figure(0)
-plt.ylabel("Regret")
+
+
+# Plot the cumulative regret
+plt.figure("Cumulative Regret")
 plt.xlabel("t")
+plt.ylabel("Cumulative Regret")
+plt.plot(np.cumsum(np.mean(opt - np.array(gpts_rewards_per_experiment),axis=0)),'r')
+plt.plot(np.cumsum(np.mean(opt - np.array(gpucb_rewards_per_experiment),axis=0)),'g')
+plt.legend(["GP-TS","GP-UCB"])
 
-plt.plot(np.cumsum(np.mean(opt-gpts_rewards_per_experiment, axis=0)),'r')
-        
 
-plt.show()       
+# plot the cumulative profit
+plt.figure('Cumulative Reward')
+plt.plot(np.cumsum(np.mean(np.array(gpts_rewards_per_experiment),axis=0)),'r')
+plt.plot(np.cumsum(np.mean(np.array(gpucb_rewards_per_experiment),axis=0)),'g')
+plt.plot(np.cumsum(np.full(T, opt)), 'b')
+plt.xlabel("t")
+plt.ylabel("Profit")
+plt.legend(["GP-TS","GP-UCB","Optimal"])
 
+# plot the Instantaneous Regret
+plt.figure("Instantaneous Regret")
+plt.xlabel("t")
+plt.ylabel("Instantaneous Regret")
+plt.plot(np.mean(opt - np.array(gpts_rewards_per_experiment),axis=0),'r')
+plt.plot(np.mean(opt - np.array(gpucb_rewards_per_experiment),axis=0),'g')
+plt.legend(["GP-TS","GP-UCB"])
 
+# plot the Instantaneous profit
+plt.figure('Instantaneous Reward')
+plt.plot(np.mean(np.array(gpts_rewards_per_experiment),axis=0),'r')
+plt.plot(np.mean(np.array(gpucb_rewards_per_experiment),axis=0),'g')
+plt.plot(np.full(T, opt), 'b')
+plt.xlabel("t")
+plt.ylabel("Profit")
+plt.legend(["GP-TS","GP-UCB","Optimal"])
+plt.show()
