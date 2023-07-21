75d4b2817c60067e34f7257acd80cd750fb1d5f9
Saad-ELH-ELB <75778999+Saad-ELH-ELB@users.noreply.github.com>
2023-07-21 22:49:02 +0200

    Update GPTS_Learner_Step2.py

--- GPTS_Learner_Step2.py
+++ GPTS_Learner_Step2.py
@@ -20,7 +20,7 @@ class GPTS_Learner(Learner):
         self.pulled_arms = []
         alpha = 0.9
         kernel = C(1,(1e-3,1e3)) * RBF(1,(1e-3,1e3))
-        self.gp = GaussianProcessRegressor(kernel=kernel, alpha=alpha**2, normalize_y = True, n_restarts_optimizer=9 )
+        self.gp = GaussianProcessRegressor(kernel=kernel, alpha=alpha, normalize_y = True, n_restarts_optimizer=9 )
         
         
     def update_observations(self, arm_idx, reward):
